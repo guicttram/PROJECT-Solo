b4091713941b09d3dff9bc66dd7edc416c7019c6
Guilherme Laptop <guicttram@gmail.com>
2020-07-01 14:52:26 -0300

    Final version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import cx_Freeze
 executables = [cx_Freeze.Executable(script='solo.py', icon='stars/solo.ico')]
 cx_Freeze.setup(
     name = 'PROJECT: Solo',
-    options = {'build_exe': {'packages':['pygame'],
+    options = {'build_exe': {'packages':['pygame', 'functions'],
     'include_files': ['stars']
     } },
     executables = executables
